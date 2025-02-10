c9d218120895d6beb36ba1b4d1075dcfb806302b
Omaro Schreuder <135833013+Omarosweekly@users.noreply.github.com>
2025-02-10 12:27:09 +0100

    Changes to DiceMachine

--- src/DiceMachine.java
+++ src/DiceMachine.java
@@ -3,40 +3,60 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class DiceMachine {
-    public int roll(int diceSides, int diceAmount) {
-        int result = 0;
-        int amountRolls = 0;
-        while (!(diceAmount == amountRolls)) {
-            amountRolls++;
-            diceRoll(diceSides);
-        }
-        return result;
-    }
+    /*The choice input if the roll be  with advantage or disadvantage
+    has not been made yet will happen in the choice menu.*/
 
-    public int finalResult(Array list, String inputTypeRoll) {
-        TypeRoll checkInput = TypeRoll.valueOf(inputTypeRoll);
-        int index = 1;
+    public int finalResult(ArrayList<Integer> list, String inputTypeRoll) {
+        /*Inventory*/
+        /*For the exercise inputTypeRoll is set to Disadvantage*/
+        inputTypeRoll = "Disadvantage";
+        TypeRoll checkInput = TypeRoll.valueOf(inputTypeRoll); //converter from String to Enum
+
+        int index = 0;
         int tmp = 0;
-        if (checkInput == TypeRoll.Advantage) {
+        /*End*/
+        if (list == null || list.isEmpty()) {
+            System.out.println("No Entry");
+            return 0;
+        } else if (list.size() == 1) {
+            tmp = list.get(0);
+            // These have not been properly looped yet
+        } else if (checkInput == TypeRoll.Advantage) {
             for (int i : list) {
                 if (i > list.get(index)) {
                     tmp = index;
                     return tmp;
                 }
+                index++;
             }
-        }
-        if (checkInput == TypeRoll.Disadvantage){
+        } else if (checkInput == TypeRoll.Disadvantage){
             for (int i : list) {
                 if (i < list.get(index)) {
                     tmp = i;
                     return tmp;
                 }
+                index++;
             }
         } return tmp;
     }
 
-    private void diceRoll(int diceSides) {
+    public ArrayList<Integer> roll(int diceSides, int diceAmount) {
+        /* Inventory */
+        int amountRolls = 0;
+        ArrayList<Integer> madeRolls = new ArrayList<>();
+        /*End*/
+        while (!(diceAmount == amountRolls)) {
+            amountRolls++;
+            int  rollResult = diceRoll(diceSides);
+            // The rolls need to be stored
+            madeRolls.add(amountRolls-1, rollResult);
+        }
+        return madeRolls;
+    }
+
+
+    private int diceRoll(int diceSides) {
         Random rollResult = new Random(diceSides);
-        rollResult.nextInt();
+        return rollResult.nextInt();
     }
 }
